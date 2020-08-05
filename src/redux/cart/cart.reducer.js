0d92502642e253e1d17b1a14fc3b9e6fb7d08c31
ChrisReutter <estaverano3@gmail.com>
2020-08-06 00:24:39 +0200

    updated redux components, shop and cart as well as data-id

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,11 @@
 import CartActionTypes from './cart.types';
+import {
+    addItemToCart
+} from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
@@ -9,8 +13,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state, hidden: !state.hidden
             }
-            default:
-                return state
+            case CartActionTypes.ADD_ITEM:
+                return {
+                    ...state,
+                    cartItems: addItemToCart(state.cartItems, action.payload)
+                }
+                default:
+                    return state;
     }
 }
 export default cartReducer;
